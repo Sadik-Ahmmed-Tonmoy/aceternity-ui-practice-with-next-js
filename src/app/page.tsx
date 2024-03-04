6c10a0a8221571ff90b1cd893ad0b7995d1e741f
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-03-04 23:51:19 +0600

    added GoogleGeminiEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ThreeDCardDemo } from '../components/ThreeDCardDemo';
+import { GoogleGeminiEffectDemo } from '@/components/GoogleGeminiEffect';
 
 const page = () => {
   return (
     <div>
+      <ThreeDCardDemo/>
+      <GoogleGeminiEffectDemo/>
       <ThreeDCardDemo/>
     </div>
   );

8f82a3860cf1577b62d54fb72c2269a6808bfdf8
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-03-05 10:11:13 +0600

    HeroParallaxDemo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { ThreeDCardDemo } from '../components/ThreeDCardDemo';
 import { GoogleGeminiEffectDemo } from '@/components/GoogleGeminiEffect';
 import { SparklesCoreConcept } from '@/components/SparklesCoreConcept';
+import { LampDemo } from '@/components/LampDemo';
+import { CardHoverEffectDemo } from '@/components/CardHoverEffectDemo';
+import { HeroScrollDemo } from '@/components/HeroScrollDemo';
+import { HeroParallaxDemo } from '@/components/HeroParallaxDemo';
 
 const page = () => {
   return (
@@ -10,6 +14,10 @@ const page = () => {
       <GoogleGeminiEffectDemo/>
       <ThreeDCardDemo/>
       <SparklesCoreConcept/>
+      <LampDemo/>
+      <CardHoverEffectDemo/>
+      <HeroScrollDemo/>
+      <HeroParallaxDemo/>
     </div>
   );
 };

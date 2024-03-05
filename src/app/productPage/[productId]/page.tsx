677641b5c6da92bd2e7e9460c1ddd95b261270cb
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-03-06 00:07:18 +0600

    type update

--- src/app/productPage/[productId]/page.tsx
+++ src/app/productPage/[productId]/page.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-
-interface RouteParams {
-  productId: string;
-}
-
-const DynamicPage: React.FC<RouteComponentProps<RouteParams>> = ({ match, location }) => {
-  const { params } = match;
-  const searchParams = new URLSearchParams(location.search);
-
-  console.log(params, searchParams.get('productId'));
-
-  return (
-    <div className='text-3xl'>
-      dynamicPage {params.productId}
-    </div>
-  );
+type Params = {
+    productId: string;
 };
 
-export default DynamicPage;
+type SearchParams = {
+    search: string;
+};
+
+const dynamicPage = ({params, searchParams} : {params: Params, searchParams: SearchParams}) => {
+    console.log(params, searchParams);
+    return (
+        <div className='text-3xl'>
+            dynamicPage {params.productId}
+        </div>
+    );
+};
+
+export default dynamicPage;
